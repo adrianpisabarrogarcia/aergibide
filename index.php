e1b386013e0e9dbdd35f25fd1e13985c15d1d8cb
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2020-11-09 13:57:59 +0100

    project structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!-- LOGIN DE USUARIOS -->
+<html lang="es">
+<header>
+    <title>Aergibide</title>
+    <link rel="icon" type="image/png" href="img/favicon.png">
+
+</header>
+<body>
+
+The content of the document......
+<?php
+
+?>
+</body>
+</html>
