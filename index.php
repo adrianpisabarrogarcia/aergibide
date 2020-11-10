516c3f478dc471f49c65e3e45661b28d66bf0f87
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2020-11-10 10:17:19 +0100

    login

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
-<!-- LOGIN DE USUARIOS -->
+            <!-- LOGIN DE USUARIOS -->
 <html lang="es">
 <header>
     <title>Aergibide</title>
+    <!-- FAVICON -->
     <link rel="icon" type="image/png" href="img/favicon.png">
+    <!-- SEO -->
+    <meta name="keywords" content="foro, aeronática, aergibide, herramientas aeronáticas"/>
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
 
 </header>
 <body>
 
-The content of the document......
-<?php
+<div class="">
+    <img src="img/logo_vertical_color.png">
+    <form action="" method="post">
+        <input type="text" name="user" id="user" class="campo" placeholder="‍💻  Correo Electrónico">
+        <input type="password" name="password" id="password" class="campo" placeholder="🗝️ Contraseña">
 
-?>
+        <button type="submit" name="entrar" id="entrar" class="boton" value="Entrar">👉 &nbsp;Entrar</button>
+        <input type="button" onclick="location.href='register.php';" value="✏️ &nbsp;Registrarse" />
+    </form>
+</div>
+
+
 </body>
 </html>
---
+++ register.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+            <!-- LOGIN DE USUARIOS -->
+<html lang="es">
+<header>
+    <title>Aergibide</title>
+    <!-- FAVICON -->
+    <link rel="icon" type="image/png" href="img/favicon.png">
+    <!-- SEO -->
+    <meta name="keywords" content="foro, aeronática, aergibide, herramientas aeronáticas"/>
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
+
+</header>
+<body>
+
+<div class="">
+    <img src="img/logo_vertical_color.png">
+    <h2>Registrarse</h2>
+    <form action="" method="post">
+        <input type="text" name="user" id="user" class="campo" placeholder="🙋‍♀️ Usuario *">
+        <input type="text" name="user" id="user" class="campo" placeholder="✍️ Nombre *">
+        <input type="text" name="user" id="user" class="campo" placeholder="✍️ Apellidos *">
+        <input type="email" name="email" id="email" class="campo" placeholder="🧑‍💻 Correo Electrónico *">
+        <input type="password" name="password" id="password" class="campo" placeholder="🗝️ Contraseña *">
+        <input type="password" name="password" id="password" class="campo" placeholder="🗝️ Repite la contraseña *">
+
+        <button type="submit" name="entrar" id="entrar" class="boton" value="Registrarse">🚪️ &nbsp;Registrarse</button>
+        <input type="button" onclick="location.href='index.php';" value="🤏️ &nbsp;Atrás" />
+
+    </form>
+</div>
+
+
+</body>
+</html>
