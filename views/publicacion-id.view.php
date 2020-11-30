1d01c208e1df50f91dd17e0dfe660c21e929017e
Adrián Pisabarro García <adrian.pisabarro.garcia@gmail.com>
2020-11-30 08:56:13 +0100

    css mejora publicacion id

--- views/publicacion-id.view.php
+++ views/publicacion-id.view.php
@@ -36,40 +36,40 @@
         <div class="cuadro_publicacion">
             <?php
 
-                $row = $publica;
-                $titulo = $row->Titulo;
-                $descripcion = $row->Descripcion;
-                $user = $row->Usuario;
-                $fecha = $row->Fecha;
-                $archivo = $row->Archivo;
-                $ID = $row->ID;
-                // $respuestas = generarRespuestas($ID, $dbh);
-                // $row2 = $respuestas->fetchAll();
-                // $numero = count($row2);
+            $row = $publica;
+            $titulo = $row->Titulo;
+            $descripcion = $row->Descripcion;
+            $user = $row->Usuario;
+            $fecha = $row->Fecha;
+            $archivo = $row->Archivo;
+            $ID = $row->ID;
+            // $respuestas = generarRespuestas($ID, $dbh);
+            // $row2 = $respuestas->fetchAll();
+            // $numero = count($row2);
 
 
-                $usuarioSesion = $_SESSION["usuario"];
+            $usuarioSesion = $_SESSION["usuario"];
 
-                ?>
+            ?>
 
-                <div class="publicacion">
-                    <div class="titulo">
-                        <h1 class="title"><?= utf8_encode($titulo) ?></h1>
-                    </div>
+            <div class="publicacion">
+                <div class="titulo">
+                    <h1 class="title"><?= utf8_encode($titulo) ?></h1>
+                </div>
 
-                    <div class="descripcion">
-                        <span><?= utf8_encode($descripcion) ?></span>
-                    </div>
+                <div class="descripcion">
+                    <span><?= utf8_encode($descripcion) ?></span>
+                </div>
 
-                    <div class="fecha">
-                        <span class="date"><?= $fecha ?></span>
-                    </div>
+                <div class="fecha">
+                    <span class="date"><?= $fecha ?></span>
+                </div>
 
-                    <div class="usuario">
-                        <span class="user"><?= utf8_encode($user) ?></span>
-                    </div>
+                <div class="usuario">
+                    <span class="user"><?= utf8_encode($user) ?></span>
+                </div>
 
-                    <?php if($user == $usuarioSesion){ ?>
+                <?php if($user == $usuarioSesion){ ?>
                     <div class="respuestas">
                         <button class="borrar_preg" value="<?= $ID ?>">
                             <svg width="1em" height="1em" viewBox="0 0 16 16" style="color: #E74C3C" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -85,10 +85,10 @@
                         -->
 
                     </div>
-                    <?php } ?>
+                <?php } ?>
 
-                </div>
-            <div class="like_fav_down">
+            </div>
+            <div class="like_fav">
                 <div class="corazon">
                     <?php
                     $mostrarLike = comprobarLike($dbh, $datos->ID, $ID);
@@ -167,82 +167,82 @@
                 <h1>No se han encontrado Respuestas</h1>
             <?php }else{ ?>
                 <h1>Respuestas:</h1>
-            <?php
+                <?php
 
                 while ($row3 = $respuest->fetch()) {
-            $descripcion = $row3->Descripcion;
-            $userResp = $row3->Usuario;
-            $fecha = $row3->Fecha;
-            $archivoRespuesta = $row3->Archivo;
-            $idRespuesta = $row3->ID;
+                    $descripcion = $row3->Descripcion;
+                    $userResp = $row3->Usuario;
+                    $fecha = $row3->Fecha;
+                    $archivoRespuesta = $row3->Archivo;
+                    $idRespuesta = $row3->ID;
 
 
 
-            ?>
+                    ?>
 
-            <div class="publicacion">
-                <div class="descripcion">
-                    <span><?= utf8_encode($descripcion) ?></span>
-                </div>
+                    <div class="publicacion">
+                        <div class="descripcion">
+                            <span><?= utf8_encode($descripcion) ?></span>
+                        </div>
 
-                <div class="fecha">
-                    <span class="date"><?= $fecha ?></span>
-                </div>
+                        <div class="fecha">
+                            <span class="date"><?= $fecha ?></span>
+                        </div>
 
-                <div class="usuario">
-                    <span class="user"><?= utf8_encode($userResp) ?></span>
-                </div>
+                        <div class="usuario">
+                            <span class="user"><?= utf8_encode($userResp) ?></span>
+                        </div>
 
-                <?php if ($userResp == $usuarioSesion || $user == $usuarioSesion){ ?>
-                <div class="respuestas">
-                    <button class="borrar" value="<?= $idRespuesta ?>">
-                        <svg width="1em" height="1em" viewBox="0 0 16 16" style="color: #E74C3C" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
-                        </svg>
-                    </button>
-                </div>
-                <?php } ?>
+                        <?php if ($userResp == $usuarioSesion || $user == $usuarioSesion){ ?>
+                            <div class="respuestas">
+                                <button class="borrar" value="<?= $idRespuesta ?>">
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" style="color: #E74C3C" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        <?php } ?>
 
-            </div>
-            <div class="like_fav_down" id="repuesta_like_fav_down">
-                <div class="corazon">
-                    <?php
-                    $mostrarLikeRespuesta = comprobarLikeRespuesta($dbh, $datos->ID, $idRespuesta );
-                    $ID_resp = 0;
-                    while($row = $mostrarLikeRespuesta->fetch()){
-                        $ID_resp = $row->ID_Respuesta;
-                    }
-                    $claseLikeRespuesta = "rosa"; ?>
-
-                    <button class="likeRespuesta" value="<?= $idRespuesta ?>">
-                        <?php  if ($ID_resp == $idRespuesta){ ?>
-                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill <?= $claseLikeRespuesta ?>"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <?php } else{ ?>
-                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill "
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <?php } ?>
-                                <path fill-rule="evenodd"
-                                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-                            </svg>
-                    </button>
-                </div>
-
-                <?php if($archivoRespuesta != ""){ ?>
-                    <div class="down_title">Descargar archivo:</div>
-                    <div class="descargar">
-                        <a href="<?= $archivoRespuesta ?>" target="_blank" style="height: 100%" download>
-                            <button class="down">
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cloud-download-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M8 0a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 4.095 0 5.555 0 7.318 0 9.366 1.708 11 3.781 11H7.5V5.5a.5.5 0 0 1 1 0V11h4.188C14.502 11 16 9.57 16 7.773c0-1.636-1.242-2.969-2.834-3.194C12.923 1.999 10.69 0 8 0zm-.354 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V11h-1v3.293l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/>
-                                </svg>
-                            </button>
-                        </a>
                     </div>
-                <?php } ?>
+                    <div class="like_fav_down" id="repuesta_like_fav_down">
+                        <div class="corazon">
+                            <?php
+                            $mostrarLikeRespuesta = comprobarLikeRespuesta($dbh, $datos->ID, $idRespuesta );
+                            $ID_resp = 0;
+                            while($row = $mostrarLikeRespuesta->fetch()){
+                                $ID_resp = $row->ID_Respuesta;
+                            }
+                            $claseLikeRespuesta = "rosa"; ?>
 
-            </div>
-            <?php }
+                            <button class="likeRespuesta" value="<?= $idRespuesta ?>">
+                                <?php  if ($ID_resp == $idRespuesta){ ?>
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill <?= $claseLikeRespuesta ?>"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <?php } else{ ?>
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill "
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <?php } ?>
+                                        <path fill-rule="evenodd"
+                                              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                    </svg>
+                            </button>
+                        </div>
+
+                        <?php if($archivoRespuesta != ""){ ?>
+                            <div class="down_title">Descargar archivo:</div>
+                            <div class="descargar">
+                                <a href="<?= $archivoRespuesta ?>" target="_blank" style="height: 100%" download>
+                                    <button class="down">
+                                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cloud-download-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M8 0a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 4.095 0 5.555 0 7.318 0 9.366 1.708 11 3.781 11H7.5V5.5a.5.5 0 0 1 1 0V11h4.188C14.502 11 16 9.57 16 7.773c0-1.636-1.242-2.969-2.834-3.194C12.923 1.999 10.69 0 8 0zm-.354 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V11h-1v3.293l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/>
+                                        </svg>
+                                    </button>
+                                </a>
+                            </div>
+                        <?php } ?>
+
+                    </div>
+                <?php }
             } ?>
 
         </div>
